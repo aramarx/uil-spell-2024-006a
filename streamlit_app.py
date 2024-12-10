a691377ef2ada0399c54a12fdfd986d072f871e0
aramarx <aramarx.us@gmail.com>
2024-12-10 12:36:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ password = st.text_input("Password", type="password")
 if st.button("Log in", type="primary"):
     if username == "test" and password == "test":
         st.session_state.logged_in = True
-        st.success("Logged in successfully!")
+        st.success("👍Logged in successfully!")
         sleep(0.5)
         st.switch_page("pages/page1.py")
     else:
